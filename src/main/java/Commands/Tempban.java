1787bbfac3218ae914c393a2e6256a8f804e6d3d
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-18 15:20:07 -0600

    Create Tempban.java

---
+++ src/main/java/Commands/Tempban.java
@@ -0,0 +1,41 @@
+package Commands;
+//Nolan Amblard did this
+import Bot.Commands;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import java.awt.*;
+import java.util.List;
+
+public class Tempban implements Commands {
+
+    @Override
+    public void execute(List<String> args, MessageReceivedEvent event) {
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        if (message.length == 1) {
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
+        }
+        if (message.length == 2) {
+            event.getChannel().sendMessage("Please include the duration you want to ban the user for (whole number of days).");
+        }
+
+        if (message.length == 3) {
+            String username = message[1];
+            int duration = Integer.parseInt(message[2]);
+            event.getGuild().getMemberByTag(username).ban(duration);
+            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days.");
+        }
+
+        if (message.length == 4) {
+            String username = message[1];
+            int duration = Integer.parseInt(message[2]);
+            String reason = message[3];
+            event.getGuild().getMemberByTag(username).ban(duration, reason);
+            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days. Reason: " + reason);
+        }
+    }
+
+    @Override
+    public String getKeyword() {
+        return "Tempban";
+    }
+}
