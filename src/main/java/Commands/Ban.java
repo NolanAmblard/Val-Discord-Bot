8b8743cce1433113b10189676c024d5d97b28c03
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-18 15:03:53 -0600

    Create Ban.java

---
+++ src/main/java/Commands/Ban.java
@@ -0,0 +1,35 @@
+package Commands;
+//Nolan Amblard did this
+import Bot.Commands;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import java.awt.*;
+import java.util.List;
+
+public class Ban implements Commands {
+
+    @Override
+    public void execute(List<String> args, MessageReceivedEvent event) {
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        if (message.length == 1) {
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
+        }
+        if (message.length == 2) {
+            String username = message[1];
+            event.getGuild().getMemberByTag(username).ban(54750);
+            event.getChannel().sendMessage("User " + username + " has been banned.");
+        }
+
+        if (message.length == 3) {
+            String username = message[1];
+            String reason = message[2];
+            event.getGuild().getMemberByTag(username).ban(54750, reason);
+            event.getChannel().sendMessage("User " + username + " has been banned. Reason: " + reason);
+        }
+    }
+
+    @Override
+    public String getKeyword() {
+        return "Ban";
+    }
+}
