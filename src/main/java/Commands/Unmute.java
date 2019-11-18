ab99637e68ac00c0c37852af9c84a2646b8e3d1f
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-18 15:04:57 -0600

    Create Unmute.java

---
+++ src/main/java/Commands/Unmute.java
@@ -0,0 +1,29 @@
+package Commands;
+//Nolan Amblard did this
+import Bot.Commands;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import java.awt.*;
+import java.util.List;
+
+public class Unmute implements Commands {
+
+    @Override
+    public void execute(List<String> args, MessageReceivedEvent event) {
+        String[] message = event.getMessage().getContentRaw().split(" ");
+        if (message.length == 1) {
+            event.getChannel().sendMessage("Please include the person you want to unmute's tag.");
+        }
+        if (message.length == 2) {
+            String username = message[1];
+            event.getGuild().getMemberByTag(username).mute(false);
+            event.getChannel().sendMessage("User " + username + " has been unmuted.");
+        }
+    }
+}
+
+    @Override
+    public String getKeyword() {
+        return "Unmute";
+    }
+}
