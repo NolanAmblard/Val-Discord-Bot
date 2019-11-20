a0e97ab59b7cc189917de2b5ada5ec329ab0ca54
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-20 14:38:50 -0600

    Update Tempban.java

--- src/main/java/Commands/Tempban.java
+++ src/main/java/Commands/Tempban.java
@@ -12,25 +12,25 @@ public class Tempban implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.").queue();
         }
         if (message.length == 2) {
-            event.getChannel().sendMessage("Please include the duration you want to ban the user for (whole number of days).");
+            event.getChannel().sendMessage("Please include the duration you want to ban the user for (whole number of days).").queue();
         }
 
         if (message.length == 3) {
             String username = message[1];
             int duration = Integer.parseInt(message[2]);
-            event.getGuild().getMemberByTag(username).ban(duration);
-            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days.");
+            event.getGuild().getMemberByTag(username).ban(duration).queue();
+            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days.").queue();
         }
 
         if (message.length == 4) {
             String username = message[1];
             int duration = Integer.parseInt(message[2]);
             String reason = message[3];
-            event.getGuild().getMemberByTag(username).ban(duration, reason);
-            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days. Reason: " + reason);
+            event.getGuild().getMemberByTag(username).ban(duration, reason).queue();
+            event.getChannel().sendMessage("User " + username + " has been banned for " + duration + " days. Reason: " + reason).queue();
         }
     }
 
