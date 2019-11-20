edfdd147db70a0e7237a6cd84f4099c208e255d5
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-20 14:32:09 -0600

    Update Ban.java

--- src/main/java/Commands/Ban.java
+++ src/main/java/Commands/Ban.java
@@ -12,11 +12,11 @@ public class Ban implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.").queue();
         }
         if (message.length == 2) {
             String username = message[1];
-            event.getGuild().getMemberByTag(username).ban(54750);
+            event.getGuild().getMemberByTag(username).ban(54750).queue();
             event.getChannel().sendMessage("User " + username + " has been banned.");
         }
 
@@ -24,7 +24,7 @@ public class Ban implements Commands {
             String username = message[1];
             String reason = message[2];
             event.getGuild().getMemberByTag(username).ban(54750, reason);
-            event.getChannel().sendMessage("User " + username + " has been banned. Reason: " + reason);
+            event.getChannel().sendMessage("User " + username + " has been banned. Reason: " + reason).queue();
         }
     }
 
