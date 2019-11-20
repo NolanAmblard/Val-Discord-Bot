5bcdecf8094c9de65a6dabc96dc316ab5be395f1
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-20 14:37:08 -0600

    Update Mute.java

--- src/main/java/Commands/Mute.java
+++ src/main/java/Commands/Mute.java
@@ -12,19 +12,19 @@ public class Mute implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to mute's tag.");
+            event.getChannel().sendMessage("Please include the person you want to mute's tag.").queue();
         }
         if (message.length == 2) {
             String username = message[1];
-            event.getGuild().getMemberByTag(username).mute(true);
-            event.getChannel().sendMessage("User " + username + " has been muted.");
+            event.getGuild().getMemberByTag(username).mute(true).queue();
+            event.getChannel().sendMessage("User " + username + " has been muted.").queue();
         }
 
         if (message.length == 3) {
             String username = message[1];
             String reason = message[2];
-            event.getGuild().getMemberByTag(username).mute(true);
-            event.getChannel().sendMessage("User " + username + " has been muted. Reason: " + reason);
+            event.getGuild().getMemberByTag(username).mute(true).queue();
+            event.getChannel().sendMessage("User " + username + " has been muted. Reason: " + reason).queue();
         }
 
         }
