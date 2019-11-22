b0d7dc29b08a46286025b36a56c5a1ad69ed7ea0
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-22 15:02:35 -0600

    Update Ban.java

--- src/main/java/Commands/Ban.java
+++ src/main/java/Commands/Ban.java
@@ -1,6 +1,7 @@
 package Commands;
 //Nolan Amblard did this
 import Bot.Commands;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.awt.*;
@@ -12,19 +13,19 @@ public class Ban implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to ban's tag.").queue();
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
         }
         if (message.length == 2) {
             String username = message[1];
-            event.getGuild().getMemberByTag(username).ban(54750).queue();
-            event.getChannel().sendMessage("User " + username + " has been banned.").queue();
+            event.getMessage().getMentionedMembers().get(0).ban(54750);
+            event.getChannel().sendMessage("User " + username + " has been banned.");
         }
 
         if (message.length == 3) {
             String username = message[1];
             String reason = message[2];
-            event.getGuild().getMemberByTag(username).ban(54750, reason).queue();
-            event.getChannel().sendMessage("User " + username + " has been banned. Reason: " + reason).queue();
+            event.getMessage().getMentionedMembers().get(0).ban(54750, reason);
+            event.getChannel().sendMessage("User " + username + " has been banned. Reason: " + reason);
         }
     }
 
