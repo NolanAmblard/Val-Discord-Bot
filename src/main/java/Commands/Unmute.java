d02d6a7ecba57b03048a6f44997bbf39dc1acd97
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-22 15:16:33 -0600

    Update Unmute.java

--- src/main/java/Commands/Unmute.java
+++ src/main/java/Commands/Unmute.java
@@ -1,6 +1,7 @@
 package Commands;
 //Nolan Amblard did this
 import Bot.Commands;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.awt.*;
@@ -12,18 +13,17 @@ public class Unmute implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to unmute's tag.").queue();
+            event.getChannel().sendMessage("Please include the person you want to unmute's tag.");
         }
         if (message.length == 2) {
-            String username = message[1];
-            event.getGuild().getMemberByTag(username).mute(false).queue();
-            event.getChannel().sendMessage("User " + username + " has been unmuted.").queue();
+
+            event.getMessage().getMentionedMembers().get(0).mute(false);
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been unmuted.");
         }
     }
-
+}
 
     @Override
     public String getKeyword() {
         return "Unmute";
     }
-}
