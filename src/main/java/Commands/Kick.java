88db9597585c3f0981991f3bb6784ef0828de26e
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-22 15:15:21 -0600

    Update Kick.java

--- src/main/java/Commands/Kick.java
+++ src/main/java/Commands/Kick.java
@@ -18,14 +18,14 @@ public class Kick implements Commands {
         if (message.length == 2) {
             String username = message[1];
             event.getGuild().getMemberByTag(username).kick();
-            event.getChannel().sendMessage("User " + username + " has been kicked.");
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been kicked.");
         }
 
         if (message.length == 3) {
             String username = message[1];
             String reason = message[2];
             event.getMessage().getMentionedMembers().get(0).kick(reason);
-            event.getChannel().sendMessage("User " + username + " has been kicked. Reason: " + reason);
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been kicked. Reason: " + reason);
         }
     }
 
