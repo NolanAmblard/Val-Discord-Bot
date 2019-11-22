42470ac6d729f6c5c0b2a09080a8298aa746716e
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-11-22 15:02:03 -0600

    Update Kick.java

--- src/main/java/Commands/Kick.java
+++ src/main/java/Commands/Kick.java
@@ -1,6 +1,7 @@
 package Commands;
 //Nolan Amblard did this
 import Bot.Commands;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import java.awt.*;
@@ -12,19 +13,19 @@ public class Kick implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to kick's tag.").queue();
+            event.getChannel().sendMessage("Please include the person you want to kick's tag.");
         }
         if (message.length == 2) {
             String username = message[1];
-            event.getGuild().getMemberByTag(username).kick().queue();
-            event.getChannel().sendMessage("User " + username + " has been kicked.").queue();
+            event.getGuild().getMemberByTag(username).kick();
+            event.getChannel().sendMessage("User " + username + " has been kicked.");
         }
 
         if (message.length == 3) {
             String username = message[1];
             String reason = message[2];
-            event.getGuild().getMemberByTag(username).kick(reason).queue();
-            event.getChannel().sendMessage("User " + username + " has been kicked. Reason: " + reason).queue();
+            event.getMessage().getMentionedMembers().get(0).kick(reason);
+            event.getChannel().sendMessage("User " + username + " has been kicked. Reason: " + reason);
         }
     }
 
