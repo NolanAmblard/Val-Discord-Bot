dcfab6e94a64c7c3b6f4203c8f501ae170052e61
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-12-02 08:33:15 -0600

    Update Tempban.java

--- src/main/java/Commands/Tempban.java
+++ src/main/java/Commands/Tempban.java
@@ -12,23 +12,23 @@ public class Tempban implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to ban's tag.");
+            event.getChannel().sendMessage("Please include the person you want to ban's tag.").queue();
         }
         if (message.length == 2) {
-            event.getChannel().sendMessage("Please include the duration you want to ban the user for (whole number of days).");
+            event.getChannel().sendMessage("Please include the duration you want to ban the user for (whole number of days).").queue();
         }
 
         if (message.length == 3) {
             int duration = Integer.parseInt(message[2]);
-            event.getMessage().getMentionedMembers().get(0).ban(duration);
-            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned for " + duration + " days.");
+            event.getMessage().getMentionedMembers().get(0).ban(duration).queue();
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned for " + duration + " days.").queue();
         }
 
         if (message.length == 4) {
             String reason = message[3];
             int duration = Integer.parseInt(message[2]);
-            event.getMessage().getMentionedMembers().get(0).ban(duration);
-            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned for " + duration + " days. Reason: " + reason);
+            event.getMessage().getMentionedMembers().get(0).ban(duration).queue();
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned for " + duration + " days. Reason: " + reason).queue();
         }
     }
 
