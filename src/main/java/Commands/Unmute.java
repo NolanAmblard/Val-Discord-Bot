1f66f0fdaaace863215f931cf2a4aed042600be4
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-12-02 08:36:49 -0600

    Update Unmute.java

--- src/main/java/Commands/Unmute.java
+++ src/main/java/Commands/Unmute.java
@@ -13,12 +13,12 @@ public class Unmute implements Commands {
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
         if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to unmute's tag.");
+            event.getChannel().sendMessage("Please include the person you want to unmute's tag.").queue();
         }
         if (message.length == 2) {
-
-            event.getMessage().getMentionedMembers().get(0).mute(false);
-            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been unmuted.");
+            //username should be in message[1]
+            event.getMessage().getMentionedMembers().get(0).mute(false).queue();
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been unmuted.").queue();
         }
     }
 
