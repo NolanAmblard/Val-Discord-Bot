518a7b863365e18545e6016241cdc6f271008717
Lawrence <lzlawrencezhang@gmail.com>
2019-12-06 15:08:51 -0600

    Updated MuteVoice and UnmuteVoice Commands (introduced try catch statements for uncaught exceptions)

--- src/main/java/Commands/MuteVoice.java
+++ src/main/java/Commands/MuteVoice.java
@@ -11,24 +11,41 @@ public class MuteVoice implements Commands {
     @Override
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
-        if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to mute's tag.").queue();
-        }
-        if (message.length == 2) {
-            //Mutes a user
-            //String username = message[1];
-            User user = event.getMessage().getMentionedUsers().get(0);
-            event.getGuild().getMember(user).mute(true).queue();
-            event.getChannel().sendMessage("User " + user.getName() + " has been muted.").queue();
-        }
 
-        if (message.length == 3) {
-            //Mutes a user for the specified reason
-            //String username = message[1];
-            String reason = message[2];
-            User user = event.getMessage().getMentionedUsers().get(0);
-            event.getGuild().getMember(user).mute(true).queue();
-            event.getChannel().sendMessage("User " + user.getName() + " has been muted. Reason: " + reason).queue();
+        try {
+            if (message.length == 1) {
+                event.getChannel().sendMessage("Please include the person you want to mute's tag.").queue();
+            }
+
+            if (message.length == 2) {
+                try {
+                    //Mutes a user
+                    //String username = message[1];
+                    User user = event.getMessage().getMentionedUsers().get(0);
+                    event.getGuild().getMember(user).mute(true).queue();
+                    event.getChannel().sendMessage("User " + user.getName() + " has been muted.").queue();
+                }
+                catch (IndexOutOfBoundsException e) {
+                    event.getChannel().sendMessage("The requested user does not exist.").queue();
+                }
+            }
+
+            if (message.length == 3) {
+                try {
+                    //Mutes a user for the specified reason
+                    //String username = message[1];
+                    String reason = message[2];
+                    User user = event.getMessage().getMentionedUsers().get(0);
+                    event.getGuild().getMember(user).mute(true).queue();
+                    event.getChannel().sendMessage("User " + user.getName() + " has been muted. Reason: " + reason).queue();
+                }
+                catch (IndexOutOfBoundsException e) {
+                    event.getChannel().sendMessage("The requested user does not exist.").queue();
+                }
+            }
+        }
+        catch (IllegalStateException e) {
+            event.getChannel().sendMessage("The requested user is not currently in a voice channel.").queue();
         }
     }
 
--- src/main/java/Commands/UnmuteVoice.java
+++ src/main/java/Commands/UnmuteVoice.java
@@ -12,16 +12,26 @@ public class UnmuteVoice implements Commands {
     @Override
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
-        if (message.length == 1) {
-            event.getChannel().sendMessage("Please include the person you want to unmute's tag.").queue();
+        try {
+            if (message.length == 1) {
+                event.getChannel().sendMessage("Please include the person you want to unmute's tag.").queue();
+            }
+            if (message.length == 2) {
+                try {
+                    //unmutes the user mentioned in the message
+                    //username should be in message[1]
+                    User user = event.getMessage().getMentionedUsers().get(0);
+                    event.getGuild().getMember(user).mute(false).queue();
+                    //event.getMessage().getMentionedUsers().get(0).mute(false).queue();
+                    event.getChannel().sendMessage("User " + user.getName() + " has been unmuted.").queue();
+                }
+                catch (IndexOutOfBoundsException e) {
+                    event.getChannel().sendMessage("The requested user does not exist.").queue();
+                }
+            }
         }
-        if (message.length == 2) {
-            //unmutes the user mentioned in the message
-            //username should be in message[1]
-            User user = event.getMessage().getMentionedUsers().get(0);
-            event.getGuild().getMember(user).mute(false).queue();
-            //event.getMessage().getMentionedUsers().get(0).mute(false).queue();
-            event.getChannel().sendMessage("User " + user.getName() + " has been unmuted.").queue();
+        catch (IllegalStateException e) {
+            event.getChannel().sendMessage("The requested user is not currently in a voice channel.").queue();
         }
     }
 
