cf54f28d56722c8be90300c713c2a6fe8af08b24
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-12-05 21:55:11 -0600

    Update Ban.java

--- src/main/java/Commands/Ban.java
+++ src/main/java/Commands/Ban.java
@@ -18,16 +18,16 @@ public class Ban implements Commands {
         if (message.length == 2) {
             //Bans a user
             String username = message[1];
-            event.getMessage().getMentionedMembers().get(0).ban(54750).queue();
             event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned.").queue();
+            event.getMessage().getMentionedMembers().get(0).ban(1).queue();
         }
 
         if (message.length == 3) {
             //Bans a user, includes a reason
             String username = message[1];
             String reason = message[2];
-            event.getMessage().getMentionedMembers().get(0).ban(54750, reason).queue();
             event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been banned. Reason: " + reason).queue();
+            event.getMessage().getMentionedMembers().get(0).ban(1, reason).queue();
         }
     }
 
