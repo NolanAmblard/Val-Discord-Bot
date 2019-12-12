fd39bfbf2d4ef0aabcd807b81942c9c0fc841c6e
NolanAmblard <57413019+NolanAmblard@users.noreply.github.com>
2019-12-12 15:09:34 -0600

    Update Mute.java

--- src/main/java/Commands/Mute.java
+++ src/main/java/Commands/Mute.java
@@ -1,6 +1,7 @@
 package Commands;
 //Nolan Amblard did this
 import Bot.Commands;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -12,33 +13,36 @@ import java.util.concurrent.TimeUnit;
 
 public class Mute implements Commands {
 
+    private int counter = 0;
+
     @Override
     public void execute(List<String> args, MessageReceivedEvent event) {
         String[] message = event.getMessage().getContentRaw().split(" ");
-        try (event.getMessage().getMentionedMembers().get(0).)
         if (message.length == 1) {
             event.getChannel().sendMessage("Please include the person you want to mute's tag.").queue();
         }
         if (message.length == 2) {
-            String username = message[1];
-            event.getGuild().addRoleToMember(event.getMessage().getMentionedMembers().get(0), (Role) event.getGuild().getRolesByName("MUTED", true));
+            //String username = message[1];
+            Member target = event.getMessage().getMentionedMembers().get(0);
+            Role muted = target.getGuild().getRolesByName("Muted", true).get(0);
+            event.getGuild().addRoleToMember(target, muted).queue();
             event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been muted.").queue();
         }
 
         if (message.length == 3) {
-            String username = message[1];
-            String duration = message[2];
-            User user = event.getMessage().getMentionedMembers().get(0).getUser();
-            event.getGuild().addRoleToMember(event.getMessage().getMentionedMembers().get(0), (Role) event.getGuild().getRolesByName("MUTED", true));
-            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been muted. Reason: " + reason).queue();
+            //String username = message[1];
+            //String duration = message[2];
+            Member target = event.getMessage().getMentionedMembers().get(0);
+            tempmute(target, parseTime(message[2]), parseTimeUnit(message[2]));
+            event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been muted.").queue();
         }
 
         if (message.length == 4) {
-            String username = message[1];
-            String duration = message[2];
+            //String username = message[1];
+            //String duration = message[2];
             String reason = message[3];
-            User user = event.getMessage().getMentionedMembers().get(0).getUser();
-            event.getGuild().getMember(user).mute(true).queue();
+            Member target = event.getMessage().getMentionedMembers().get(0);
+            tempmute(target, parseTime(message[2]), parseTimeUnit(message[2]));
             event.getChannel().sendMessage("User " + event.getMessage().getMentionedMembers().get(0).getNickname() + " has been muted. Reason: " + reason).queue();
         }
     }
@@ -111,12 +115,19 @@ public class Mute implements Commands {
     }
 
     private void tempmute(Member target, int time, TimeUnit unit){
+        Role muted = target.getGuild().getRolesByName("Muted", true).get(0);
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run()
             {
-                
+                counter++;
+                target.getGuild().addRoleToMember(target, muted).queue();
+                if (counter == 2) {
+                    target.getGuild().removeRoleFromMember(target, muted).queue();
+                    this.cancel();
+                }
+
             }
         };
         switch(unit) {
