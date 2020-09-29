facd4917349b400f778281aa909465c49ad20f1e
Lawrence <lzlawrencezhang@gmail.com>
2020-09-28 20:29:13 -0500

    Implemented WordDictionary and added Gson dependency

--- src/main/java/Commands/WordDictionary.java
+++ src/main/java/Commands/WordDictionary.java
@@ -7,19 +7,68 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import org.jsoup.Jsoup;
+import com.google.gson.Gson;
+
+import java.awt.*;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class WordDictionary implements Commands {
 
+    static class Word {
+        private String definition;
+        private String word;
+        private Meaning[] meanings;
+
+        static class Meaning {
+            private String partOfSpeech;
+            private Definition[] definitions;
+
+            static class Definition {
+                private String definition;
+                private String example;
+            }
+        }
+    }
+
     @Override
     public void execute(List<String> args, MessageReceivedEvent event) {
         Message m = event.getMessage();
-        String[] content = m.getContentRaw().toLowerCase().split(" ");
+        String raw = m.getContentRaw();
+        String content = raw.substring(raw.indexOf(" ") + 1);
         MessageChannel channel = event.getChannel();
         EmbedBuilder embedBuilder = new EmbedBuilder();
 
         try {
-            channel.sendMessage("Work in Progress!").queue();
+            String request = Jsoup.connect("https://api.dictionaryapi.dev/api/v2/entries/en/" + URLEncoder.encode(content, "UTF-8")).userAgent("Chrome").ignoreContentType(true).execute().body();
+            request = request.substring(1, request.length() - 1);
+            Word word = new Gson().fromJson(request, Word.class);
+
+            embedBuilder.setTitle(word.word);
+
+            for (int i = 0; i < word.meanings.length; i++) {
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                for (int j = 0; j < word.meanings[i].definitions.length; j++) {
+                    stringBuilder.append(word.meanings[i].definitions[j].definition).append("\n");
+
+                    if (word.meanings[i].definitions[j].example != null) {
+                        stringBuilder.append("\"").append(word.meanings[i].definitions[j].example).append("\"");
+                    }
+
+                    if (j != word.meanings[i].definitions.length - 1) {
+                        stringBuilder.append("\n\n");
+                    }
+                }
+
+                embedBuilder.addField(word.meanings[i].partOfSpeech, stringBuilder.toString(), false);
+            }
+
+            embedBuilder.setColor(Color.CYAN);
+
+            event.getChannel().sendMessage(embedBuilder.build()).queue();
         }
         catch (Exception e) {
             channel.sendMessage("Sorry, that's either not a word or an invalid input.").queue();
