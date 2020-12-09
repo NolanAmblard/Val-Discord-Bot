180705138366c60dcf960cba365716e4acf9a415
Lawrence <lzlawrencezhang@gmail.com>
2020-12-09 02:11:33 -0600

    Updated JDA and optimized Main Bot

--- src/main/java/Bot/ValBot.java
+++ src/main/java/Bot/ValBot.java
@@ -4,24 +4,20 @@ package Bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+
+import java.io.FileInputStream;
+import java.util.Properties;
 
 //Based on Noah Liu's Discord Bot for implementation of commands and getting user input for ease of use
 public class ValBot extends ListenerAdapter {
 
-    private ValBot() {
-        CommandManager cm = new CommandManager();
-        Listener listener = new Listener(cm);
-
-        try {
-            new DefaultShardManagerBuilder().setToken("NjM2NjU5MTQ5ODk1MzY4NzQ2.Xb-IlQ.0bNZnwVhsQjTwuRNpKIH8u-Ej4I").addEventListeners(listener).build();
-        }
-        catch (Exception e) {
-            e.getMessage();
-        }
-    }
     public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("config.properties");
+        properties.load(fileInputStream);
 
-        JDA bot = new JDABuilder("NjM2NjU5MTQ5ODk1MzY4NzQ2.Xb-IlQ.0bNZnwVhsQjTwuRNpKIH8u-Ej4I").addEventListeners(new ValBot()).build();
+        CommandManager cm = new CommandManager();
+
+        JDA bot = JDABuilder.createDefault(properties.getProperty("token")).addEventListeners(new Listener(cm)).build();
     }
 }
